7838ee98f9c7f52b0172b715cf0cbbbf885c79f8
Elliot Huck <39445681+elliot-huck@users.noreply.github.com>
2018-05-24 14:56:06 -0500

    Commented index & added delete functionality

--- main.js
+++ main.js
@@ -5,18 +5,31 @@ const createCard = () => {
   const textInput = document.querySelector("#input").value;
   const newCard = document.createElement("div");
   const firstChild = outputDiv.firstElementChild;
+
+  // creates the card
   newCard.className = "card";
   newCard.innerHTML = `
-    <input type="color">
-    <input type="color">
+    <input type="color" class="background-color">
+    <input type="color" class="text-color">
     <p>${textInput}</p>
     <button class="delete">Delete</button>
   `;
+
+  // inserts the newest card at the beginning of the outputDiv
   if (outputDiv.firstElementChild === null) {
     outputDiv.appendChild(newCard);
   } else {
     outputDiv.insertBefore(newCard, firstChild);
   }
+
+  // adds function deleteCard to the delete button
+  newCard.querySelector(".delete").addEventListener("click", deleteCard)
+  
+}
+
+const deleteCard = function() {
+  const parentCard = this.parentNode;
+  outputDiv.removeChild(parentCard);
 }
 
 createButton.addEventListener("click", createCard);
